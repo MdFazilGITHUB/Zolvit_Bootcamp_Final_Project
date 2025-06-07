2fed291badd8583900c0b9f41b727818f748da00
MdFazilGITHUB <mmilyas540@gmail.com>
2025-06-07 12:09:34 +0530

    day 16 authorization and filtering using user token

--- models/taskModel.js
+++ models/taskModel.js
@@ -11,7 +11,8 @@ const taskSchema = new mongoose.Schema(
     dueDate: { type: Date },
     userRef: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
+      ref: "User",
+      required: true
     }
   },
   { timestamps: true }
